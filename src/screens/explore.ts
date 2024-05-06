99484c2232962f43ac6272faa87f861aea2828f5
25natalia <25natalia09garcia@gmail.com>
2024-05-06 15:32:33 -0500

    avances

--- src/screens/explore.ts
+++ src/screens/explore.ts
@@ -1,5 +1,7 @@
 import { AttributesFriends } from '../components/friends/friends';
+import { AttributePerfil } from '../components/perfil/perfil';
 import { getFriends } from '../services/Firebase';
+import { getSongs } from '../services/Firebase';
 
 export class Explore extends HTMLElement {
 	constructor() {
@@ -8,19 +10,31 @@ export class Explore extends HTMLElement {
 	}
 
 	connectedCallback() {
-		this.render();
+		this.renderFriends();
+		this.renderSongs();
 	}
 
-	async render() {
+	async renderFriends() {
 		if (this.shadowRoot) {
 			const dataFriends = await getFriends();
 			dataFriends.forEach((friend: any) => {
 				const myFriend = document.createElement('my-friend');
 				myFriend.setAttribute(AttributesFriends.name, friend.name);
 				myFriend.setAttribute(AttributesFriends.profile, friend.profile);
-        myFriend.setAttribute(AttributesFriends.photo, friend.photo);
-        myFriend.setAttribute(AttributesFriends.song, friend.song);
+				myFriend.setAttribute(AttributesFriends.photo, friend.photo);
+				myFriend.setAttribute(AttributesFriends.song, friend.song);
 				this.shadowRoot?.appendChild(myFriend);
+			});
+		}
+	}
+
+	async renderSongs() {
+		if (this.shadowRoot) {
+			const dataSongs = await getSongs();
+			dataSongs.forEach((song: any) => {
+				const mySong = document.createElement('my-perfil');
+				mySong.setAttribute(AttributePerfil.song_title, song.song_title);
+				this.shadowRoot?.appendChild(mySong);
 			});
 		}
 	}
