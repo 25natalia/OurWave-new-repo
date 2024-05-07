de854c6704f5d49780a5b90c4ecab21bc48a87d2
Emy Dorado <mac@MacBook-Air-de-Emy.local>
2024-05-07 13:00:47 -0500

    estilos perfil

--- src/components/profile/profile.ts
+++ src/components/profile/profile.ts
@@ -1,3 +1,5 @@
+import styles from './profile.css';
+
 export enum AttributeProfile {
 	'profile_image' = 'profile_image',
 	'username' = 'username',
@@ -47,16 +49,27 @@ class Perfil extends HTMLElement {
 	render() {
 		if (this.shadowRoot) {
 			this.shadowRoot.innerHTML = `
-    <img src="${this.profile_image}"></img>
-    <p>${this.username}</p>
-    <p>${this.name}</p>
-    <p><svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M15 11.586V6h2V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2h2v5.586l-2.707 1.707A.996.996 0 0 0 6 14v2a1 1 0 0 0 1 1h4v3l1 2 1-2v-3h4a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L15 11.586z"></path></svg></svg>
+		<section class="profile">
+    <img class="image" src="${this.profile_image}"></img>
+    <h1>${this.username}</h1>
+		<Button class="edit">Edit picture</Button>
+		<p class="fav_song"><svg class="pin"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgb(46, 196, 182);transform: ;msFilter:;"><path d="M15 11.586V6h2V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2h2v5.586l-2.707 1.707A.996.996 0 0 0 6 14v2a1 1 0 0 0 1 1h4v3l1 2 1-2v-3h4a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L15 11.586z"></path></svg></svg>
 		${this.fav_song}</p>
+
+		<section class="name-following">
+    <p class="name">${this.name}</p>
+		<p class="following">${this.following}</p>
+		</section>
+
     <p>${this.followers}</p>
-    <p>${this.following}</p>
     <Button>Playlists</Button>
+		</section>
     `;
 		}
+
+		const cssProfile = this.ownerDocument.createElement('style');
+		cssProfile.innerHTML = styles;
+		this.shadowRoot?.appendChild(cssProfile);
 	}
 }
 
