751ef6e5b6844e9b349a786454c1898456470667
25natalia <25natalia09garcia@gmail.com>
2024-05-08 14:22:55 -0500

    solucion error de merge

--- src/screens/explore.ts
+++ src/screens/explore.ts
@@ -65,7 +65,6 @@ export class Explore extends HTMLElement {
 			myFriend.setAttribute(AttributesFriends.song, friend.song);
 			this.shadowRoot?.appendChild(myFriend);
 		});
-<<<<<<< HEAD
 
 		const cssExplore = this.ownerDocument.createElement('style');
 		cssExplore.innerHTML = styles;
@@ -74,8 +73,3 @@ export class Explore extends HTMLElement {
 }
 customElements.define('app-explore', Explore);
 
-=======
-	}
-}
-customElements.define('app-explore', Explore);
->>>>>>> f9b1046bafcaec106cb451df1fe3173908d844e5
