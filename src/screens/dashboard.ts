e6cd1ff5d0c257563be8a9087734be4aee770079
25natalia <25natalia09garcia@gmail.com>
2024-05-09 22:41:25 -0500

    renderiza en orden

--- src/screens/dashboard.ts
+++ src/screens/dashboard.ts
@@ -7,115 +7,109 @@ import { addcontent } from '../services/dataAdd';
 import { profile } from '../services/dataProfile';
 import { AttributesCard } from '../components/card/card';
 import { addObserver, appState, dispatch } from '../store';
-import Firebase, {addWave} from '../services/Firebase';
-import {waves} from '../types/waves'
+import Firebase, { addWave } from '../services/Firebase';
+import { waves } from '../types/waves';
 
 const formData: Omit<waves, 'id'> = {
-    wave:''
+	wave: '',
 };
 
 export class Dashboard extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        addObserver(this);
-    }
+	waves: string[] = []; // Array para almacenar todas las olas ingresadas
 
-    connectedCallback() {
-        this.render();
-    }
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		addObserver(this);
+	}
 
-    render() {
-        if (this.shadowRoot) {
-            // Render initial content
-            header.forEach((iconoHeader) => {
-                const myHeader = document.createElement('my-header');
-                myHeader.setAttribute(AttributesHeader.logo, iconoHeader.logo);
-                this.shadowRoot?.appendChild(myHeader);
-            });
+	connectedCallback() {
+		this.render();
+	}
 
-            const enterWave = this.ownerDocument.createElement('input');
-            enterWave.placeholder = 'Enter your Wave';
-            enterWave.classList.add('image');
-            this.shadowRoot.appendChild(enterWave);
+	render() {
+		if (this.shadowRoot) {
+			// Render initial content
+			header.forEach((iconoHeader) => {
+				const myHeader = document.createElement('my-header');
+				myHeader.setAttribute(AttributesHeader.logo, iconoHeader.logo);
+				this.shadowRoot?.appendChild(myHeader);
+			});
 
-            const save = this.ownerDocument.createElement('button');
-            save.innerText = 'Save';
-            save.classList.add('save');
-            save.addEventListener('click', this.submitForm.bind(this)); // Bind 'this' context
-            this.shadowRoot?.appendChild(save);
+			// Render input field
+			const enterWave = this.ownerDocument.createElement('input');
+			enterWave.placeholder = 'Enter your Wave';
+			enterWave.classList.add('image');
+			this.shadowRoot.appendChild(enterWave);
 
-            profile.forEach((element) => {
-                const myCard = document.createElement('my-card');
-                console.log(element);
-                myCard.setAttribute(AttributesCard.name, element.name);
-                myCard.setAttribute(AttributesCard.image, element.image);
-                myCard.setAttribute(AttributesCard.wave, element.wave);
-                myCard.setAttribute(AttributesCard.unlike, element.unlike);
-                myCard.setAttribute(AttributesCard.like, element.like);
-                myCard.setAttribute(AttributesCard.cantidadlike, element.cantidadlike);
-                myCard.setAttribute(AttributesCard.share, element.share);
-                myCard.setAttribute(AttributesCard.cantidadshare, element.cantidadshare);
-                myCard.setAttribute(AttributesCard.comentar, element.comentar);
-                myCard.setAttribute(AttributesCard.cantidadcomentar, element.cantidadcomentar);
+			// Render save button
+			const save = this.ownerDocument.createElement('button');
+			save.innerText = 'Save';
+			save.classList.add('save');
+			save.addEventListener('click', this.submitForm.bind(this)); // Bind 'this' context
+			this.shadowRoot?.appendChild(save);
 
-                this.shadowRoot?.appendChild(myCard);
-            });
+			// Render todas las olas previamente guardadas al cargar la página
+			const savedWaves = localStorage.getItem('savedWaves');
+			if (savedWaves) {
+				this.waves = JSON.parse(savedWaves);
+				this.waves.forEach((wave) => {
+					this.renderWave(wave);
+				});
+			}
 
-            iconos.forEach((iconoData) => {
-                const myIcono = document.createElement('my-iconos');
-                myIcono.setAttribute(Attribute.iconohome, iconoData.iconohome);
-                myIcono.setAttribute(Attribute.iconoexplore, iconoData.iconoexplore);
-                myIcono.setAttribute(Attribute.iconoprofile, iconoData.iconoprofile);
-                this.shadowRoot?.appendChild(myIcono);
-            });
+			// Render profile cards
+			profile.forEach((element) => {
+				const myCard = document.createElement('my-card');
+				console.log(element);
+				myCard.setAttribute(AttributesCard.name, element.name);
+				myCard.setAttribute(AttributesCard.image, element.image);
+				myCard.setAttribute(AttributesCard.wave, element.wave);
+				myCard.setAttribute(AttributesCard.unlike, element.unlike);
+				myCard.setAttribute(AttributesCard.like, element.like);
+				myCard.setAttribute(AttributesCard.cantidadlike, element.cantidadlike);
+				myCard.setAttribute(AttributesCard.share, element.share);
+				myCard.setAttribute(AttributesCard.cantidadshare, element.cantidadshare);
+				myCard.setAttribute(AttributesCard.comentar, element.comentar);
+				myCard.setAttribute(AttributesCard.cantidadcomentar, element.cantidadcomentar);
+				this.shadowRoot?.appendChild(myCard);
+			});
+		}
+	}
 
-            // Render previously saved wave text
-            if (localStorage.getItem('savedWave')) {
-                const savedWave = localStorage.getItem('savedWave')!;
-                const waveDisplay = document.createElement('p');
-                waveDisplay.textContent = savedWave;
-                if (enterWave && enterWave.nextSibling) {
-                    this.shadowRoot.insertBefore(waveDisplay, enterWave.nextSibling);
-                } else {
-                    this.shadowRoot.appendChild(waveDisplay);
-                }
-            }
-        }
-    }
+	submitForm() {
+		// Get the value of the input field
+		const enterWaveInput = this.shadowRoot?.querySelector('.image') as HTMLInputElement;
+		const waveText = enterWaveInput.value;
 
-    submitForm() {
-        // Get the value of the input field
-        const enterWaveInput = this.shadowRoot?.querySelector('.image') as HTMLInputElement;
-        const waveText = enterWaveInput.value;
+		// Agregar la ola al array y al almacenamiento local al principio
+		this.waves.unshift(waveText); // Agregar al principio del array
+		localStorage.setItem('savedWaves', JSON.stringify(this.waves)); // Save waves
 
-        // Set the value of the input field in the formData
-        formData.wave = waveText;
+		// Set the value of the input field in the formData
+		formData.wave = waveText;
 
-        // Log formData
-        console.log(formData);
+		// Log formData
+		console.log(formData);
 
-        // Call addWave method with formData
-        Firebase.addWave(formData);
+		// Call addWave method with formData
+		Firebase.addWave(formData);
 
-        // Clear the input field
-        enterWaveInput.value = '';
+		// Render the entered wave text below other content
+		this.renderWave(waveText);
 
-        // Save the entered wave text to localStorage
-        localStorage.setItem('savedWave', waveText);
+		// Clear the input field
+		enterWaveInput.value = '';
+	}
 
-        // Render the entered wave text above other content
-        const waveDisplay = document.createElement('p');
-        waveDisplay.textContent = waveText;
-
-        // Insert the waveDisplay element
-        if (this.shadowRoot && enterWaveInput && enterWaveInput.nextSibling) {
-            this.shadowRoot.insertBefore(waveDisplay, enterWaveInput.nextSibling);
-        } else {
-            // Handle the case when elements are not available
-            console.error("Failed to insert waveDisplay element: required elements are not available.");
-        }
-    }
+	renderWave(waveText: string) {
+		const waveDisplay = document.createElement('p');
+		waveDisplay.textContent = waveText;
+		// Obtener el botón de guardar
+		const saveButton = this.shadowRoot?.querySelector('.save') as HTMLButtonElement;
+		// Insertar la nueva ola justo antes del botón de guardar
+		this.shadowRoot?.insertBefore(waveDisplay, saveButton.nextSibling);
+	}
 }
 
 customElements.define('app-dashboard', Dashboard);
